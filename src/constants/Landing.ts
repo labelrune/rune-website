f3a6a77a0d8decf6cd60eea0fbb5b1140d361de7
Anway <31155434+anwaysomani@users.noreply.github.com>
2025-08-18 16:44:20 +0530

    Update Landing.ts

--- src/constants/Landing.ts
+++ src/constants/Landing.ts
@@ -3,17 +3,17 @@ import { LandingImageProps } from "src/types/Landing";
 export const LandingImages: LandingImageProps[] = [
   {
     id: 1,
-    src: "https://wallpaperbat.com/img/59089186-x-cactus-jack-x-nike-wallpaper-i.jpg",
+    src: "https://i.ibb.co/gLsR19J0/3.webp",
     alt: "Cactus Jack Nike Wallpaper",
   },
   {
     id: 2,
-    src: "https://wallpaperaccess.com/full/369659.jpg",
+    src: "https://i.ibb.co/RGV1QyBN/2.webp",
     alt: "Addidas",
   },
   {
     id: 3,
-    src: "https://wallpapercave.com/wp/57e4hub.jpg",
+    src: "https://i.ibb.co/DfbKt0b1/image.webp",
     alt: "Puma",
   },
 ];

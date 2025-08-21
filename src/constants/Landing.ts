2bc078977868939ef8dacaeff6d5879b04a5da80
Anway <31155434+anwaysomani@users.noreply.github.com>
2025-08-21 16:30:57 +0530

    Update Landing.ts

--- src/constants/Landing.ts
+++ src/constants/Landing.ts
@@ -3,32 +3,27 @@ import { LandingImageProps } from "src/types/Landing";
 export const LandingImages: LandingImageProps[] = [
   {
     id: 1,
-    src: "https://i.ibb.co/BH7f3Fxc/test01.webp",
-    alt: "Rune1",
+    src: "https://i.ibb.co/svQ4bCfW/rune-banner-1.webp",
+    alt: "Rune Banner 1",
   },
   {
     id: 2,
-    src: "https://i.ibb.co/BH7f3Fxc/test01.webp",
-    alt: "Rune2",
+    src: "https://i.ibb.co/WNwmZB4x/rune-banner-2.webp",
+    alt: "Rune Banner 2",
   },
   {
     id: 3,
-    src: "https://i.ibb.co/BH7f3Fxc/test01.webp",
-    alt: "Rune3",
+    src: "https://i.ibb.co/20dLDv38/rune-banner-3.webp",
+    alt: "Rune Banner 3",
   },
   {
     id: 4,
-    src: "https://i.ibb.co/Xxt6tK2J/JEM1528-copy-2.jpg",
-    alt: "Rune4",
+    src: "https://i.ibb.co/4RFnHjxH/rune-banner-4.webp",
+    alt: "Rune Banner 4",
   },
   {
     id: 5,
-    src: "https://i.ibb.co/mCy0fJNB/JEM00338-copy-2.jpg",
-    alt: "Rune5",
-  },
-  {
-    id: 6,
-    src: "https://i.ibb.co/zjmk1LW/JEM00868-copy-3.jpg",
-    alt: "Rune6",
-  },
+    src: "https://i.ibb.co/3mzTJTpy/rune-banner-5.webp",
+    alt: "Rune Banner 5",
+  }
 ];

e7291e8978e03c1b218f56597ff8530ccc52988a
Anway <31155434+anwaysomani@users.noreply.github.com>
2025-08-21 14:13:28 +0530

    Update Landing.ts

--- src/constants/Landing.ts
+++ src/constants/Landing.ts
@@ -3,17 +3,32 @@ import { LandingImageProps } from "src/types/Landing";
 export const LandingImages: LandingImageProps[] = [
   {
     id: 1,
-    src: "https://i.ibb.co/gLsR19J0/3.webp",
+    src: "https://i.ibb.co/C5xFBbSv/IMG-2923-jpg-N.jpg",
     alt: "Rune1",
   },
   {
     id: 2,
-    src: "https://i.ibb.co/RGV1QyBN/2.webp",
+    src: "https://i.ibb.co/whrMT4xJ/95-D3115-D-389-D-4002-8-BE4-4-F7-D7575-ED2-E.jpg",
     alt: "Rune2",
   },
   {
     id: 3,
-    src: "https://i.ibb.co/DfbKt0b1/image.webp",
+    src: "https://i.ibb.co/HTQFJy3J/5664-AABB-CBA7-4-B7-B-9465-1576-AA007-CDC.jpg",
     alt: "Rune3",
+  },
+  {
+    id: 4,
+    src: "https://i.ibb.co/Xxt6tK2J/JEM1528-copy-2.jpg",
+    alt: "Rune4",
+  },
+  {
+    id: 5,
+    src: "https://i.ibb.co/mCy0fJNB/JEM00338-copy-2.jpg",
+    alt: "Rune5",
+  },
+  {
+    id: 6,
+    src: "https://i.ibb.co/zjmk1LW/JEM00868-copy-3.jpg",
+    alt: "Rune6",
   },
 ];

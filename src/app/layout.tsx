884a634193b2bf7b0ce1371c4587e0add3956a45
Anway <31155434+anwaysomani@users.noreply.github.com>
2025-08-27 18:20:37 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,8 @@ import Link from "next/link";
 import FacebookPixel from "src/molecules/common/FacebookPixel";
 import { Suspense } from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Rune",
@@ -26,28 +19,21 @@ const MetaPixelID = process.env.META_PIXEL_ID ?? "";
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body
-        className={⁠ ${geistSans.variable} ${geistMono.variable} antialiased ⁠}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
         <div className="pt-28">{children}</div>
         <Footer />
         <Link
-          href="https://wa.me/+919008255433?text=Hey!"
+          href="https://wa.me/919008255433?text=Hey!"
           target="_blank"
+          rel="noopener noreferrer"
           className="fixed bottom-8 right-8 md:bottom-16 md:right-16 animate-pulse hover:animate-none w-12 md:w-16 aspect-square"
           aria-label="rune-whatsapp"
         >
-          <img
-            src="/whatsapp.svg"
-            alt="whatsapp_image"
-            className="w-full h-full object-cover"
-          />
+          <img src="/whatsapp.svg" alt="whatsapp_image" className="w-full h-full object-cover" />
         </Link>
         <Suspense fallback={null}>
           <FacebookPixel pixelID={MetaPixelID} />

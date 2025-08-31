563e8a5f2c6b3edc343e357b043967ea40d72f58
Anway <31155434+anwaysomani@users.noreply.github.com>
2025-08-31 12:48:18 +0530

    Update Landing.ts

--- src/constants/Landing.ts
+++ src/constants/Landing.ts
@@ -3,8 +3,32 @@ import { LandingImageProps } from "src/types/Landing";
 export const LandingImages: LandingImageProps[] = [
   {
     id: 1,
-    src: "https://s3.ap-south-1.amazonaws.com/www.ultraviolette.com/temp-2/bnr-2_11zon.webp",//"https://i.ibb.co/WNwmZB4x/rune-banner-2.webp",
+    src: "https://i.ibb.co/svQ4bCfW/rune-banner-1.webp",
     alt: "Rune Banner 1",
     src_mobile: "https://i.ibb.co/nFfVH1z/image.jpg",
-  }
+  },
+  {
+    id: 2,
+    src: "https://i.ibb.co/WNwmZB4x/rune-banner-2.webp",
+    alt: "Rune Banner 2",
+    src_mobile: "https://i.ibb.co/rKGbhRrf/1.jpg",
+  },
+  {
+    id: 3,
+    src: "https://i.ibb.co/20dLDv38/rune-banner-3.webp",
+    alt: "Rune Banner 3",
+    src_mobile: "https://i.ibb.co/35Yps5BT/2.jpg",
+  },
+  {
+    id: 4,
+    src: "https://i.ibb.co/4RFnHjxH/rune-banner-4.webp",
+    alt: "Rune Banner 4",
+    src_mobile: "https://i.ibb.co/MkpcmPGW/3.jpg",
+  },
+  {
+    id: 5,
+    src: "https://i.ibb.co/3mzTJTpy/rune-banner-5.webp",
+    alt: "Rune Banner 5",
+    src_mobile: "https://i.ibb.co/rKGbhRrf/1.jpg",
+  },
 ];

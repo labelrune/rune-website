82ed839ca9b6ec0492c56e696807876ef4c11f6d
Anway <31155434+anwaysomani@users.noreply.github.com>
2025-08-31 12:41:33 +0530

    Update Landing.ts

--- src/constants/Landing.ts
+++ src/constants/Landing.ts
@@ -6,29 +6,5 @@ export const LandingImages: LandingImageProps[] = [
     src: "https://s3.ap-south-1.amazonaws.com/www.ultraviolette.com/temp-2/bnr-2_11zon.webp",//"https://i.ibb.co/WNwmZB4x/rune-banner-2.webp",
     alt: "Rune Banner 1",
     src_mobile: "https://i.ibb.co/nFfVH1z/image.jpg",
-  },
-  {
-    id: 2,
-    src: "https://s3.ap-south-1.amazonaws.com/www.ultraviolette.com/temp-2/bnr-2_11zon.webp",//"https://i.ibb.co/WNwmZB4x/rune-banner-2.webp",
-    alt: "Rune Banner 2",
-    src_mobile: "https://i.ibb.co/rKGbhRrf/1.jpg",
-  },
-  {
-    id: 3,
-    src: "https://s3.ap-south-1.amazonaws.com/www.ultraviolette.com/temp-2/bnr-2_11zon.webp",//"https://i.ibb.co/WNwmZB4x/rune-banner-2.webp",
-    alt: "Rune Banner 3",
-    src_mobile: "https://i.ibb.co/35Yps5BT/2.jpg",
-  },
-  {
-    id: 4,
-    src: "https://s3.ap-south-1.amazonaws.com/www.ultraviolette.com/temp-2/bnr-2_11zon.webp",//"https://i.ibb.co/WNwmZB4x/rune-banner-2.webp",
-    alt: "Rune Banner 4",
-    src_mobile: "https://i.ibb.co/MkpcmPGW/3.jpg",
-  },
-  {
-    id: 5,
-    src: "https://s3.ap-south-1.amazonaws.com/www.ultraviolette.com/temp-2/bnr-2_11zon.webp",//"https://i.ibb.co/WNwmZB4x/rune-banner-2.webp",
-    alt: "Rune Banner 5",
-    src_mobile: "https://i.ibb.co/rKGbhRrf/1.jpg",
-  },
+  }
 ];
